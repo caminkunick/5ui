08bbf3a468929dc356f4cd6619716081706c8e08
caminkunick <caminkunick@gmail.com>
2024-01-16 20:12:26 +0700

    re-order action in postScript.js

--- postScript.js
+++ postScript.js
@@ -10,14 +10,6 @@ packageJson.name = project_name;
 fs.writeFileSync('./package.json', JSON.stringify(packageJson, null, 2));
 console.log('package.json name changed');
 
-// remove folder .git
-fs.rmdirSync('.git', { recursive: true });
-execSync('git init');
-execSync('git add .');
-execSync('git commit -m "initial commit"');
-execSync('git branch -M main');
-console.log('git initialized');
-
 // replace index.html %SITE_NAME% with project name
 const indexHtml = fs.readFileSync(path.join(__dirname, './public/index.html')).toString();
 fs.writeFileSync(path.join(__dirname, './public/index.html'), indexHtml.replace('%SITE_NAME%', project_name));
@@ -27,6 +19,14 @@ console.log('index.html changed');
 const readme = fs.readFileSync(path.join(__dirname, './README.md')).toString();
 fs.writeFileSync(path.join(__dirname, './README.md'), readme.replace('%PROJECT_NAME%', project_name));
 console.log('README.md changed');
+
+// remove folder .git
+fs.rmdirSync('.git', { recursive: true });
+execSync('git init');
+execSync('git add .');
+execSync('git commit -m "initial commit"');
+execSync('git branch -M main');
+console.log('git initialized');
 
 // remove postScript.js
 fs.unlinkSync('./postScript.js');
