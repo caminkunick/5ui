0efd0c4ca717b2f379a848491b962a2287d15ebd
caminkunick <caminkunick@gmail.com>
2024-01-16 20:00:41 +0700

    update postScript.js

--- postScript.js
+++ postScript.js
@@ -1,25 +1,20 @@
 const fs = require('fs');
 const { execSync } = require('child_process');
 
-// remove folder .git
-fs.rmdirSync('.git', { recursive: true });
-
-// init git
-execSync('git init');
-
-// add all files
-execSync('git add .');
-
-// commit
-execSync('git commit -m "initial commit"');
-
-// change branch name to main
-execSync('git branch -M main');
-
 // change name in package.json to folder name
 const packageJson = JSON.parse(fs.readFileSync('./package.json'));
 packageJson.name = __dirname.split('/').pop();
 fs.writeFileSync('./package.json', JSON.stringify(packageJson, null, 2));
+console.log('package.json name changed');
+
+// remove folder .git
+fs.rmdirSync('.git', { recursive: true });
+execSync('git init');
+execSync('git add .');
+execSync('git commit -m "initial commit"');
+execSync('git branch -M main');
+console.log('git initialized');
 
 // remove postScript.js
 fs.unlinkSync('./postScript.js');
+console.log('postScript.js removed');
