c4a12d5d0267a674d32e8fb64db3d95132fcbbb8
caminkunick <caminkunick@gmail.com>
2024-01-16 19:56:25 +0700

    add action to postScript.js

--- postScript.js
+++ postScript.js
@@ -1,4 +1,25 @@
 const fs = require('fs');
+const { execSync } = require('child_process');
 
-console.log('Post-create script: Running after project creation.');
-fs.writeFileSync('post-script.log', 'This is a post-create script.');
+// remove folder .git
+fs.rmdirSync('.git', { recursive: true });
+
+// init git
+execSync('git init');
+
+// add all files
+execSync('git add .');
+
+// commit
+execSync('git commit -m "initial commit"');
+
+// change branch name to main
+execSync('git branch -M main');
+
+// change name in package.json to folder name
+const packageJson = JSON.parse(fs.readFileSync('./package.json'));
+packageJson.name = __dirname.split('/').pop();
+fs.writeFileSync('./package.json', JSON.stringify(packageJson, null, 2));
+
+// remove postScript.js
+fs.unlinkSync('./postScript.js');
